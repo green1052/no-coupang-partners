47817a82794e89a6d52960f99de1175c86da9663
green1052 <34261355+green1052@users.noreply.github.com>
2022-06-01 02:32:01 +0900

    1

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const {EOL} = require("os");
 
-const REGEX = fs.readFileSync("./hosts.txt", "utf8").split("\r\n").join("|");
+const REGEX = fs.readFileSync("./hosts.txt", "utf8").split(EOL).join("|");
 
 fs.writeFileSync(`filters-share/search.txt`, `google.*#?#div[role="main"] div#search div[data-async-context] div[data-hveid]:-abp-contains(/${REGEX}/)
 duckduckgo.com#?#div.result:-abp-contains(/${REGEX}/)`, "utf8");

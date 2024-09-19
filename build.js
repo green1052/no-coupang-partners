2641419df77237b5c3be0cc41bb7b4a319370f62
green1052 <green1052@pm.me>
2024-09-20 00:39:07 +0900

    Update build.js

--- build.js
+++ build.js
@@ -7,5 +7,5 @@ const REGEX = fs.readFileSync("./hosts.txt", "utf8")
     .join("|")
     .replaceAll(".", "\\.");
 
-fs.writeFileSync(`filters-share/search.txt`, `google.*#?#:is(div#search, div#botstuff) div[data-async-context] div.g[data-hveid]:-abp-contains(/${REGEX}/)
+fs.writeFileSync(`filters-share/search.txt`, `google.*#?#:is(div#search, div#botstuff) div[data-async-context] div.g[data-ved]:-abp-contains(/${REGEX}/)
 duckduckgo.com#?#div.results article:-abp-contains(/${REGEX}/)`, "utf8");
